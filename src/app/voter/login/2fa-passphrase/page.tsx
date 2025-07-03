555571457cce1433b774c548e962d02092fac229
Marc King <marclawrenceking@gmail.com>
2025-07-03 16:49:37 +0800

    election status & nilipat ng route yung voterlogin to app/voter/login

--- src/app/voter/login/2fa-passphrase/page.tsx
+++ src/app/voter/login/2fa-passphrase/page.tsx
@@ -1,23 +1,24 @@
 import Button from "@/components/Button";
 import Image from "next/image";
 
-import EnterEmail from "../../assets/EnterEmail.png";
+import EnterPassphrase from "../../../assets/EnterPassphrase.png";
 import OtpInput from "@/components/OtpInput";
-const Email2fa = () => {
+const Passphrase2fa = () => {
   return (
     <main className=" flex flex-col items-center justify-center gap-10 px-10 py-20">
       <div className="text-center space-y-2">
-        <p className="voterlogin-heading">Enter your email</p>
+        <p className="voterlogin-heading">Enter passphrase</p>
         <p className="voterlogin-subheading">
-          Enter your email address or log in with Google to verify that the
-          email we sent belongs to you.
+          Enter your Passphrase to continue. This was sent to your{" "}
+          <span className="text-primary">email</span> and will serve as your
+          authentication for this session.
         </p>
       </div>
 
-      <Image src={EnterEmail} height={300} alt="BotoMoToLogo" />
+      <Image src={EnterPassphrase} height={300} alt="BotoMoToLogo" />
       <div className="flex flex-col gap-2">
         {" "}
-        <p className="voterlogin-label">Email Address TEXT INPUT</p>
+        <p className="voterlogin-label">PARAPHRASE TEXT INPUT</p>
         <OtpInput length={4} />
       </div>
 
@@ -29,4 +30,4 @@ const Email2fa = () => {
   );
 };
 
-export default Email2fa;
+export default Passphrase2fa;
