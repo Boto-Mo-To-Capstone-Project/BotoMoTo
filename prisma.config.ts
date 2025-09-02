ef36626c44ac028e0e59bf76ff0a3fd2c1939c84
ayumi <brianmaysebastian@gmail.com>
2025-09-02 17:26:26 +0800

    fix vercel error about prisma config

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-// prisma.config.ts
-import { defineConfig } from '@prisma/config';
-import * as dotenv from 'dotenv';
-
-dotenv.config(); // <-- make sure .env is loaded
-
-export default defineConfig({
-  schema: 'src/lib/db/schema.prisma',
-});
