d9885fb5e74a8c68c0946c8c461e3e091c89e565
ayumi <brianmaysebastian@gmail.com>
2025-09-05 00:16:27 +0800

    reset db and make email uniq oer electionid

--- src/lib/db/migrations/20250828122835_remove_islive/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isLive` on the `Election` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Election" DROP COLUMN "isLive";
--- src/lib/db/migrations/20250904121004_remove_isnew_from_candidate/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isNew` on the `Candidate` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."Candidate" DROP COLUMN "isNew";
