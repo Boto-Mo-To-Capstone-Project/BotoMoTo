c3ef3d3480076b8c93ca88b9bfdf24b60ab1f4e7
ayumi <brianmaysebastian@gmail.com>
2025-10-09 22:36:35 +0800

    add metadata

---
+++ src/app/metadata.ts
@@ -0,0 +1,7 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Boto Mo To - Secure Online Voting System',
+  description: 'Boto Mo To, Boses Mo To! A secure, fast, and reliable online voting system for small organizations.',
+  keywords: ['online voting', 'filipino voters', 'secure voting', 'boto-mo-to', 'boto mo to', 'election system', 'voting platform', 'boto-mo-to.online', 'boto mo to online', 'BotoMoTo'],
+}
